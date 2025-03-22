8bf5efc0e5988ba48fc9f761aa0ac1cb97098629
IamDushu <hey.dushyanth@gmail.com>
2025-03-22 17:04:08 +0530

    Filter messaging channels private and public

--- src/app/(app)/(tabs)/messages/index.tsx
+++ src/app/(app)/(tabs)/messages/index.tsx
@@ -1,10 +1,20 @@
+import { useAuth } from "@/context/auth";
 import { router } from "expo-router";
 
 import { ChannelList } from "stream-chat-expo";
 
 export default function Messages() {
+  const { user } = useAuth();
+
+  const isPrivate = {
+    type: "messaging",
+    members: { $in: [user.id.toString()] },
+  };
+  const isPublic = { type: "livestream" };
+
   return (
     <ChannelList
+      filters={{ $or: [isPrivate, isPublic] }}
       onSelect={(channel) => router.push(`/messages/channel/${channel.id}`)}
     />
   );
